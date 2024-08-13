a1f80a3852dfba63b1b366ebf4cf9b71ce3675de
Daniela Casas <SENA@ARMAPRCCIFOD605.FormacionSena.RED>
2024-08-13 09:53:04 -0500

    Primera parte de maquetación

---
+++ scripts/app.js
@@ -0,0 +1,8 @@
+function createCardProduct (productos) {
+    const containerProduct = document.createElement ('div')
+    containerProduct.classList.add ('product')
+    containerProduct.id = productos.id
+
+    const imgProd = document.createElement ('img')
+    imgProd.src = icecream
+}
---
+++ scripts/products.js
@@ -0,0 +1,163 @@
+export const productos = [
+    {
+        id: "1",
+        name: "Figura de mona china roja",
+        image: "img/fig1.png",
+        precio: "",
+        description: "Esta es una figura de un personaje",
+        quantity: ""
+    },
+
+    {
+        id: "2",
+        name: "Otra figura de mona china",
+        image: "img/fig2.png",
+        description: "",
+        quantity: ""
+    },
+    
+    {
+        id: "3",
+        name: "Figura de Sans",
+        image: "img/SansFig.jpg",
+        description: "",
+        quantity: ""
+    },
+
+    {
+        id: "4",
+        name: "Lámpara de Hollow knight",
+        image: "img/HollowLampara.JPG",
+        description: "",
+        quantity: ""
+    },
+
+    {
+        id: "5",
+        name: "Camisa de Hollow Knight",
+        image: "img/HollowT-shirt.jpg",
+        description: "",
+        quantity: ""
+    },
+
+    {
+        id: "6",
+        name: "Camisón de Hollow Knight",
+        image: "img/HollowT-shirt2.PNG",
+        description: "",
+        quantity: ""
+    },
+
+    {
+        id: "7",
+        name: "Figura de Itadori",
+        image: "img/ItadoriFig.jpg",
+        description: "",
+        quantity: ""
+    },
+
+    {
+        id: "8",
+        name: "Figura de Jotaro",
+        image: "img/JotaroFig.PNG",
+        description: "",
+        quantity: ""
+    },
+
+    {
+        id: "9",
+        name: "Figura de Kakyoin",
+        image: "img/KakyoinFig.PNG",
+        description: "",
+        quantity: ""
+    },
+
+    {
+        id: "10",
+        name: "Figura de Rengoku",
+        image: "img/RengokuFig.jpg",
+        description: "",
+        quantity: ""
+    },
+
+    {
+        id: "11",
+        name: "Llavero de Sans",
+        image: "img/SansLlavero.jpg",
+        description: "",
+        quantity: ""
+    },
+
+    {
+        id: "12",
+        name: "Cuadro de Sans",
+        image: "img/sansPicture.jpg",
+        description: "",
+        quantity: ""
+    },
+
+    {
+        id: "13",
+        name: "",
+        image: "",
+        description: "",
+        quantity: ""
+    },
+
+    {
+        id: "14",
+        name: "",
+        image: "",
+        description: "",
+        quantity: ""
+    },
+
+    {
+        id: "15",
+        name: "",
+        image: "",
+        description: "",
+        quantity: ""
+    },
+
+    {
+        id: "16",
+        name: "",
+        image: "",
+        description: "",
+        quantity: ""
+    },
+
+    {
+        id: "17",
+        name: "",
+        image: "",
+        description: "",
+        quantity: ""
+    },
+
+    {
+        id: "18",
+        name: "",
+        image: "",
+        description: "",
+        quantity: ""
+    },
+
+    {
+        id: "19",
+        name: "",
+        image: "",
+        description: "",
+        quantity: ""
+    },
+
+    {
+        id: "20",
+        name: "",
+        image: "",
+        description: "",
+        quantity: ""
+    },
+
+]
